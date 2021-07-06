ebf59242e62a79fe97622cba44ab8a9ca03ed119
Einar Næss Jensen <einar.nass.jensen@ntnu.no>
2021-07-06 23:01:32 +0200

    updated to latest

--- lunsj.py
+++ lunsj.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python
+#!/usr/bin/env python
 
 """reads a lustre jobstatsfile from all oss"""
 
@@ -22,13 +22,13 @@ def read_yaml(newdatafile):
     """ A function to read YAML file"""
     with open(newdatafile) as f:
         config = yaml.safe_load(f)
-
+ 
     return config
 
 
 
 def main():
-"""
+  """
   this script will need to be called with an optional argument, defining the path to the ${datadir} data directory where jobstats and slurm squeue output is
 
   Relies on having aquired jobstats from lustre with:
@@ -46,7 +46,7 @@ def main():
   I sincerely apologize for poorly written python, but this works for me. It is what it is
 
 
-"""
+  """
 
 
 # config file and config layout
@@ -70,6 +70,8 @@ def main():
         }
     }
   ]
+
+
 
 ###
 ###    Read config file
@@ -95,14 +97,14 @@ def main():
   if len(sys.argv)-1 > 0:
     datadir=sys.argv[1]
   else:
-
+      
     datadir = '/root/einjen/data/20210704-2055/'
 
   if not path.isdir(datadir):
     print('no valid datadirectory')
     sys.exit()
-
-
+ 
+ 
   datafile = os.path.join(datadir,data[0]['settings']['datafile'])
   newdatafile = os.path.join(datadir,data[0]['settings']['newdatafile'])
   servicedatafile = os.path.join(datadir,data[0]['settings']['servicedatafile'])
@@ -110,8 +112,9 @@ def main():
   servicesummary = os.path.join(datadir,data[0]['settings']['servicesummary'])
   jobsummary = os.path.join(datadir,data[0]['settings']['jobsummary'])
 
-  servicenodes =  data[0]['settings']['servicenodes']
-
+  servicenodes =  data[0]['settings']['servicenodes'] 
+  rootnodefilter =  data[0]['settings']['rootnodefilter'] 
+  
 
   a_logger = logging.getLogger()
   a_logger.setLevel(logging.DEBUG)
@@ -122,12 +125,14 @@ def main():
   a_logger.addHandler(output_file_handler)
   a_logger.addHandler(stdout_handler)
 
-
+  
   ################
   ###  CONFIG DONE
   
-  
-  
+
+
+
+
   with open(datafile) as fh:
     jobdata = fh.read()
 
@@ -135,7 +140,7 @@ def main():
   recs = [x for x in re.split('\s-\s', jobdata)]
   servicerecs = [s for s in recs if any(i in s for i in servicenodes)]
   #print(servicerecs)
-  kept = [r for r in recs if '0:b' not in r]
+  kept = [r for r in recs if rootnodefilter not in r]
   with open(newdatafile, 'w') as fh2:
     fh2.write('\n- '.join(kept))
 
@@ -176,13 +181,14 @@ def main():
         if job.has_key('write_bytes'):
           newwritesample = datalist[userjob][0]+job['write_bytes']['samples']
           newwritesum = datalist[userjob][0]+job['write_bytes']['sum']
-        newsampletotal = newwritesample+newreadsample
+        newsampletotal = newwritesample+newreadsample 
         datalist[userjob] = [newreadsample,newreadsum,newwritesample,newwritesum,newsampletotal]
-
+ 
   a_logger.debug('{:20} {:>15} {:>15} {:>15} {:>15}'.format('JOB_ID:USR_ID', 'read samples','write samples','samples total','read bytes','write bytes'))
   a_logger.debug('------------------------------------------------------------------------------------------------------------------')
   for key,value in sorted(datalist.items(), key=lambda e: e[1][2],reverse=True):
     a_logger.debug('{:20} {:15} {:15} {:15} {:15} {:15}'.format(key, value[0],value[2],value[4],value[1],value[3]))
+
 
 # servicenodes
 
@@ -197,24 +203,24 @@ def main():
   for job in jobs:
     if job['job_id'] not in datalist:
       datalist[job['job_id']] = [0,0,0,0,0]
-
+      
     newreadsample = 0
     newreadsum =0
     newwritesample = 0
     newwritesum = 0
     newsampletotal = 0
-
+    
     if job.has_key('read_bytes'):
       newreadsample = datalist[job['job_id']][0]+job['read_bytes']['samples']
       newreadsum = datalist[job['job_id']][0]+job['read_bytes']['sum']
     if job.has_key('write_bytes'):
       newwritesample = datalist[job['job_id']][0]+job['write_bytes']['samples']
       newwritesum = datalist[job['job_id']][0]+job['write_bytes']['sum']
-    newsampletotal = newwritesample+newreadsample
+    newsampletotal = newwritesample+newreadsample 
     datalist[job['job_id']] = [newreadsample,newreadsum,newwritesample,newwritesum,newsampletotal]
-
+ 
   a_logger.debug("")
-  a_logger.debug("Servicenodes Summary")
+  a_logger.debug("Servicenodes Summary") 
   a_logger.debug('{:20} {:>15} {:>15} {:>15} {:>15}'.format('USR_ID:  NODE', 'read samples','write samples','samples total','read bytes','write bytes'))
   a_logger.debug('------------------------------------------------------------------------------------------------------------------')
   for key,value in sorted(datalist.items(), key=lambda e: e[1][4],reverse=True):
@@ -224,7 +230,6 @@ def main():
 
 
 if __name__== "__main__":
-        main()
-
+	main()
 
 
